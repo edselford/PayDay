779a7b7523154d23114534330983d8aaf7d7d6a5
bal-19 <iqbalhaidee19@gmail.com>
2025-01-13 17:55:34 +0700

    add new column

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('age');
+            $table->enum("gender", ["m", "f"]);
+            $table->date("born_date");
+            $table->foreignId("role_id")->constrained("roles")->onDelete("cascade");
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
