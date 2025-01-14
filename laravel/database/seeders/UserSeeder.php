a4a70f502492b8578666703d92ba1328273e5370
bal-19 <iqbalhaidee19@gmail.com>
2025-01-14 21:26:17 +0700

    first commit

--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -15,27 +15,47 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name' => 'Edsel',
+            'age' => 99,
+            'gender' => 'm',
+            'born_date' => '2025-01-09',
+            'role_id' => 1,
             'email' => 'edselmustapa@gmail.com',
-            'password' => 'edselmustapa',
-            'role' => 'manager'
-        ]);
-
-        User::create([
-            'name' => 'Izzadin',
-            'email' => 'izzadin@gmail.com',
-            'password' => 'izzadin123'
+            'password' => bcrypt('edselmustapa')
         ]);
 
         User::create([
             'name' => 'Iqbal',
+            'age' => 19,
+            'gender' => 'm',
+            'born_date' => '2006-01-09',
+            'role_id' => 3,
             'email' => 'iqbalhaidee@gmail.com',
-            'password' => 'iqbal123'
+            'password' => bcrypt('ball19')
         ]);
 
-        User::create([
-            'name' => 'Arif',
-            'email' => 'ryvexc@gmail.com',
-            'password' => 'ryve1234'
-        ]);
+        // User::create([
+        //     'name' => 'Edsel',
+        //     'email' => 'edselmustapa@gmail.com',
+        //     'password' => 'edselmustapa',
+        //     'role' => 'manager'
+        // ]);
+
+        // User::create([
+        //     'name' => 'Izzadin',
+        //     'email' => 'izzadin@gmail.com',
+        //     'password' => 'izzadin123'
+        // ]);
+
+        // User::create([
+        //     'name' => 'Iqbal',
+        //     'email' => 'iqbalhaidee@gmail.com',
+        //     'password' => 'iqbal123'
+        // ]);
+
+        // User::create([
+        //     'name' => 'Arif',
+        //     'email' => 'ryvexc@gmail.com',
+        //     'password' => 'ryve1234'
+        // ]);
     }
 }
