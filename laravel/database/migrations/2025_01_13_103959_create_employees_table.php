be71ac735a3f4b3cfacbd1a4f9019749e791b2b5
Dinzadin <153895119+Dinzadin@users.noreply.github.com>
2025-01-19 11:15:59 +0700

    update migrate employees;

--- laravel/database/migrations/2025_01_13_103959_create_employees_table.php
+++ laravel/database/migrations/2025_01_13_103959_create_employees_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId("user_id")->constrained("users")->onDelete("cascade");
             $table->float("salary");
-            $table->string("salary_method");
+            $table->string("salary_method")->nullable();
             $table->date("join_date");
             $table->timestamps();
         });
